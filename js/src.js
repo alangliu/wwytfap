1ffb089f09b14714f94c5be5a9429cbb7cdbf9e5
alangliu <alangliu@gmail.com>
2015-10-21 07:26:34 -0500

    put in new images

--- js/src.js
+++ js/src.js
@@ -1,13 +1,393 @@
 var trade = [
     {
-        url: "Leaf.png",
-        name: "Aiden Zucker",
-        item: "Two Leafs"
+        url: "objects/o1.png",
+        name: "R. Shannon",
+        item: "Poster from Bowles Plaza"
     },
     {
-        url: "alan.png",
-        name: "Alan Liu",
-        item: "Skateboard"
+        url: "objects/o2.png",
+        name: "B. Katz",
+        item: "Marxist Poster"
+    },
+    {
+        url: "objects/o3.png",
+        name: "A. Cho",
+        item: "Hotel Lotion and Hello Kitty Card"
+    },
+        {
+        url: "objects/o4.png",
+        name: "S. Bernstein",
+        item: "Coffee Card"
+    },
+        {
+        url: "objects/o5.png",
+        name: "J. Reisman",
+        item: "Box of Cookies"
+    },
+        {
+        url: "objects/o6.png",
+        name: "N. Silverman",
+        item: "Lens Cloth"
+    },
+        {
+        url: "objects/o7.png",
+        name: "J Collins",
+        item: "Test Tube + Post-Its"
+    },
+        {
+        url: "objects/o8.png",
+        name: "R. Nickel",
+        item: "Tissue"
+    },
+        {
+        url: "objects/o9.png",
+        name: "K. Zara",
+        item: "Plastic Bag"
+    },
+        {
+        url: "objects/o10.png",
+        name: "C. Mai",
+        item: "Eco-to-Go Keychain"
+    },
+        {
+        url: "objects/o11.png",
+        name: "S. Meinecke",
+        item: "Tampon"
+    },
+        {
+        url: "objects/o12.png",
+        name: "C. Elmoro",
+        item: "Portion of a Stick"
+    },
+        {
+        url: "objects/o13.png",
+        name: "B. Luo",
+        item: "Crumpled Paper"
+    },
+        {
+        url: "objects/o14.png",
+        name: "W. Tucker",
+        item: "Coffee Stirrer"
+    },
+        {
+        url: "objects/o15.png",
+        name: "W. Bai",
+        item: "A dollar"
+    },
+        {
+        url: "objects/o16.png",
+        name: "C. Hart",
+        item: "Phone # Found on Ground"
+    },
+        {
+        url: "objects/o17.png",
+        name: "A. Krussel",
+        item: "English Breakfast Tea Bag"
+    },
+        {
+        url: "objects/o18.png",
+        name: "K. Mowery",
+        item: "Outdated Flyer"
+    },
+        {
+        url: "objects/o19.png",
+        name: "Z. Lailari",
+        item: "2 Cough Drops"
+    },
+        {
+        url: "objects/o20.png",
+        name: "J. Zhao",
+        item: "A Letter"
+    },
+        {
+        url: "objects/o21.png",
+        name: "Z. Li",
+        item: "Acorn"
+    },
+        {
+        url: "objects/o22.png",
+        name: "B. Vetuba",
+        item: "Folder"
+    },
+        {
+        url: "objects/o23.png",
+        name: "A. Mense",
+        item: "Paper Strips"
+    },
+        {
+        url: "objects/o24.png",
+        name: "R. Mirka",
+        item: "Leaf"
+    },
+        {
+        url: "objects/o25.png",
+        name: "M. O'Neill",
+        item: "Leaf"
+    },
+        {
+        url: "objects/o26.png",
+        name: "J. Mersmian",
+        item: "A Blue Pen"
+    },
+        {
+        url: "objects/o27.png",
+        name: "B. Baker",
+        item: "Business Card"
+    },
+        {
+        url: "objects/o28.png",
+        name: "A. Robinson",
+        item: "Button"
+    },
+        {
+        url: "objects/o29.png",
+        name: "E. Riley",
+        item: "Quarter"
+    },
+        {
+        url: "objects/o30.png",
+        name: "E. Roedl",
+        item: "Pen"
+    },
+        {
+        url: "objects/o31.png",
+        name: "J. Zacks",
+        item: "Pencil"
+    },
+        {
+        url: "objects/o32.png",
+        name: "S. Bhatnagar",
+        item: "Pencil"
+    },
+        {
+        url: "objects/o33.png",
+        name: "C. Scott",
+        item: "Tea Bag"
+    },
+        {
+        url: "objects/o34.png",
+        name: "E. Thomas",
+        item: "Leaf"
+    },
+        {
+        url: "objects/o35.png",
+        name: "M. Edelman",
+        item: "Hot Glue Stick (Cold)"
+    },
+        {
+        url: "objects/o36.png",
+        name: "H. Waldman",
+        item: "20 Pence"
+    },
+        {
+        url: "objects/o37.png",
+        name: "A. Nicholson",
+        item: "Cocont Water Cap"
+    },
+        {
+        url: "objects/o38.png",
+        name: "M. K",
+        item: "Quantum HW"
+    },
+        {
+        url: "objects/o39.png",
+        name: "D. Yang",
+        item: "Leaf"
+    },
+        {
+        url: "objects/o40.png",
+        name: "M. Meyer",
+        item: "Gum"
+    },
+        {
+        url: "objects/o41.png",
+        name: "S. Ragsdale",
+        item: "Business Cards (Taxi)"
+    },
+        {
+        url: "objects/o42.png",
+        name: "M. Mazza",
+        item: "Kaldi's Coffee Stamp Card"
+    },
+        {
+        url: "objects/o43.png",
+        name: "M. Gibson",
+        item: "Old Envelope"
+    },
+        {
+        url: "objects/o44.png",
+        name: "E. Ngui",
+        item: "Pencil"
+    },
+        {
+        url: "objects/o45.png",
+        name: "T. Rhodes",
+        item: "Safari Pass"
+    },
+        {
+        url: "objects/o46.png",
+        name: "B. Perlman",
+        item: "Cliff Bar Wrapper"
+    },
+        {
+        url: "objects/o47.png",
+        name: "H. Batista",
+        item: "Movie Ticket (The Martian)"
+    },
+        {
+        url: "objects/o48.png",
+        name: "L. Chen",
+        item: "Mini Binder Clip"
+    },
+        {
+        url: "objects/o49.png",
+        name: "S. Berman",
+        item: "Pencil"
+    },
+        {
+        url: "objects/o50.png",
+        name: "I. Abdikarim",
+        item: "SJC Pen"
+    },
+        {
+        url: "objects/o51.png",
+        name: "T. Cohen",
+        item: "Clevis Pin Sleeve from a Sailboat Shackle"
+    },
+        {
+        url: "objects/o52.png",
+        name: "Ken",
+        item: "News Paper Clippings"
+    },
+        {
+        url: "objects/o53.png",
+        name: "C. Chute",
+        item: "Coupon"
+    },
+        {
+        url: "objects/o54.png",
+        name: "C. Martain",
+        item: "Student Life"
+    },
+        {
+        url: "objects/o55.png",
+        name: "E. Bates",
+        item: "Coupon"
+    },
+        {
+        url: "objects/o56.png",
+        name: "S. Rey",
+        item: "Art History Flash Cards"
+    },
+        {
+        url: "objects/o57.png",
+        name: "V. Gainor",
+        item: "Painted Lightbulb"
+    },
+        {
+        url: "objects/o58.png",
+        name: "A. Kay",
+        item: "Eraser Pen"
+    },
+        {
+        url: "objects/o59.png",
+        name: "E. Lim",
+        item: "Movie Coupon"
+    },
+        {
+        url: "objects/o60.png",
+        name: "S. Nagase",
+        item: "Banana Sticker"
+    },
+        {
+        url: "objects/o61.png",
+        name: "L. Drummond",
+        item: "Pizza My Heart Card, Several Stamps"
+    },
+        {
+        url: "objects/o62.png",
+        name: "J. Pifer.",
+        item: "Part of the Previous Participant's Pancake, Pancake #61"
+    },
+        {
+        url: "objects/o63.png",
+        name: "G. Phillips",
+        item: "A Listerine Strip"
+    },
+        {
+        url: "objects/o64.png",
+        name: "L. Sullivan",
+        item: "Library Receipt"
+    },
+        {
+        url: "objects/o65.png",
+        name: "M. Magnell",
+        item: "Old Metro Pass"
+    },
+        {
+        url: "objects/o66.png",
+        name: "R. Duffy",
+        item: "Lego Joker Keychain"
+    },
+        {
+        url: "objects/o67.png",
+        name: "Z. Kline",
+        item: "Roll of 120 Film"
+    },
+        {
+        url: "objects/o68.png",
+        name: "Y. Ogi",
+        item: "President Lincoln"
+    },
+        {
+        url: "objects/o69.png",
+        name: "M. Shpuntoff",
+        item: "Free Admission to Kemper for U + a Date"
+    },
+        {
+        url: "objects/o70.png",
+        name: "G. Brion",
+        item: "All the Art Fall 15 Issue"
+    },
+        {
+        url: "objects/o71.png",
+        name: "P. Witt",
+        item: "A Bandaid and a Piece of Gum"
+    },
+        {
+        url: "objects/o72.png",
+        name: "C. Brewer",
+        item: "Red Pilor Razor"
+    },
+        {
+        url: "objects/o73.png",
+        name: "M. Weinberg",
+        item: "Plexiglass Test Cut"
+    },
+        {
+        url: "objects/o74.png",
+        name: "J. Hendrix",
+        item: "Bike Reflector"
+    },
+        {
+        url: "objects/o75.png",
+        name: "S. Lee",
+        item: "Glue Stick"
+    },
+        {
+        url: "objects/o76.png",
+        name: "P. Masini",
+        item: "Rubber Ring"
+    },
+        {
+        url: "objects/o77.png",
+        name: "S. Djajasudarma",
+        item: "Clear Tape"
+    },
+        {
+        url: "objects/o78.png",
+        name: "E. Mogavero",
+        item: "1/2 Sketchbook Project"
     }
 ]
 
@@ -27,6 +407,6 @@ $('#exampleModal').on('show.bs.modal', function (event) {
     var modal = $(this);
     modal.find('.modal-title').text(info.name);
     modal.find('.modal-item').text(info.item);
-    modal.find('.modal-image').prepend('<img src=\"' + url + '\" />')
+    modal.find('.modal-image').prepend('<img class="item-image" src=\"' + url + '\" />')
 })
 
