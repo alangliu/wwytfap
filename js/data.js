ec81625486c7431f54644330cbb6dcc0b6363e2d
zlailari <lailari.zeev@gmail.com>
2015-10-21 23:14:05 -0400

    added pancake url for each item in trade array and moved it to a new file

---
+++ js/data.js
@@ -0,0 +1,470 @@
+var trade = [
+    {
+        "url":"objects/o1.png",
+        "name":"R. Shannon",
+        "item":"Poster from Bowles Plaza",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o2.png",
+        "name":"B. Katz",
+        "item":"Marxist Poster",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o3.png",
+        "name":"A. Cho",
+        "item":"Hotel Lotion and Hello Kitty Card",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o4.png",
+        "name":"S. Bernstein",
+        "item":"Coffee Card",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o5.png",
+        "name":"J. Reisman",
+        "item":"Box of Cookies",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o6.png",
+        "name":"N. Silverman",
+        "item":"Lens Cloth",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o7.png",
+        "name":"J Collins",
+        "item":"Test Tube + Post-Its",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o8.png",
+        "name":"R. Nickel",
+        "item":"Tissue",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o9.png",
+        "name":"K. Zara",
+        "item":"Plastic Bag",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o10.png",
+        "name":"C. Mai",
+        "item":"Eco-to-Go Keychain",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o11.png",
+        "name":"S. Meinecke",
+        "item":"Tampon",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o12.png",
+        "name":"C. Elmoro",
+        "item":"Portion of a Stick",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o13.png",
+        "name":"B. Luo",
+        "item":"Crumpled Paper",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o14.png",
+        "name":"W. Tucker",
+        "item":"Coffee Stirrer",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o15.png",
+        "name":"W. Bai",
+        "item":"A dollar",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o16.png",
+        "name":"C. Hart",
+        "item":"Phone # Found on Ground",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o17.png",
+        "name":"A. Krussel",
+        "item":"English Breakfast Tea Bag",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o18.png",
+        "name":"K. Mowery",
+        "item":"Outdated Flyer",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o19.png",
+        "name":"Z. Lailari",
+        "item":"2 Cough Drops",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o20.png",
+        "name":"J. Zhao",
+        "item":"A Letter",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o21.png",
+        "name":"Z. Li",
+        "item":"Acorn",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o22.png",
+        "name":"B. Vetuba",
+        "item":"Folder",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o23.png",
+        "name":"A. Mense",
+        "item":"Paper Strips",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o24.png",
+        "name":"R. Mirka",
+        "item":"Leaf",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o25.png",
+        "name":"M. O'Neill",
+        "item":"Leaf",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o26.png",
+        "name":"J. Mersmian",
+        "item":"A Blue Pen",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o27.png",
+        "name":"B. Baker",
+        "item":"Business Card",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o28.png",
+        "name":"A. Robinson",
+        "item":"Button",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o29.png",
+        "name":"E. Riley",
+        "item":"Quarter",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o30.png",
+        "name":"E. Roedl",
+        "item":"Pen",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o31.png",
+        "name":"J. Zacks",
+        "item":"Pencil",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o32.png",
+        "name":"S. Bhatnagar",
+        "item":"Pencil",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o33.png",
+        "name":"C. Scott",
+        "item":"Tea Bag",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o34.png",
+        "name":"E. Thomas",
+        "item":"Leaf",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o35.png",
+        "name":"M. Edelman",
+        "item":"Hot Glue Stick (Cold)",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o36.png",
+        "name":"H. Waldman",
+        "item":"20 Pence",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o37.png",
+        "name":"A. Nicholson",
+        "item":"Cocont Water Cap",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o38.png",
+        "name":"M. K",
+        "item":"Quantum HW",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o39.png",
+        "name":"D. Yang",
+        "item":"Leaf",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o40.png",
+        "name":"M. Meyer",
+        "item":"Gum",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o41.png",
+        "name":"S. Ragsdale",
+        "item":"Business Cards (Taxi)",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o42.png",
+        "name":"M. Mazza",
+        "item":"Kaldi's Coffee Stamp Card",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o43.png",
+        "name":"M. Gibson",
+        "item":"Old Envelope",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o44.png",
+        "name":"E. Ngui",
+        "item":"Pencil",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o45.png",
+        "name":"T. Rhodes",
+        "item":"Safari Pass",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o46.png",
+        "name":"B. Perlman",
+        "item":"Cliff Bar Wrapper",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o47.png",
+        "name":"H. Batista",
+        "item":"Movie Ticket (The Martian)",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o48.png",
+        "name":"L. Chen",
+        "item":"Mini Binder Clip",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o49.png",
+        "name":"S. Berman",
+        "item":"Pencil",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o50.png",
+        "name":"I. Abdikarim",
+        "item":"SJC Pen",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o51.png",
+        "name":"T. Cohen",
+        "item":"Clevis Pin Sleeve from a Sailboat Shackle",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o52.png",
+        "name":"Ken",
+        "item":"News Paper Clippings",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o53.png",
+        "name":"C. Chute",
+        "item":"Coupon",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o54.png",
+        "name":"C. Martain",
+        "item":"Student Life",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o55.png",
+        "name":"E. Bates",
+        "item":"Coupon",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o56.png",
+        "name":"S. Rey",
+        "item":"Art History Flash Cards",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o57.png",
+        "name":"V. Gainor",
+        "item":"Painted Lightbulb",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o58.png",
+        "name":"A. Kay",
+        "item":"Eraser Pen",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o59.png",
+        "name":"E. Lim",
+        "item":"Movie Coupon",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o60.png",
+        "name":"S. Nagase",
+        "item":"Banana Sticker",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o61.png",
+        "name":"L. Drummond",
+        "item":"Pizza My Heart Card, Several Stamps",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o62.png",
+        "name":"J. Pifer.",
+        "item":"Part of the Previous Participant's Pancake, Pancake #61",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o63.png",
+        "name":"G. Phillips",
+        "item":"A Listerine Strip",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o64.png",
+        "name":"L. Sullivan",
+        "item":"Library Receipt",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o65.png",
+        "name":"M. Magnell",
+        "item":"Old Metro Pass",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o66.png",
+        "name":"R. Duffy",
+        "item":"Lego Joker Keychain",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o67.png",
+        "name":"Z. Kline",
+        "item":"Roll of 120 Film",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o68.png",
+        "name":"Y. Ogi",
+        "item":"President Lincoln",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o69.png",
+        "name":"M. Shpuntoff",
+        "item":"Free Admission to Kemper for U + a Date",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o70.png",
+        "name":"G. Brion",
+        "item":"All the Art Fall 15 Issue",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o71.png",
+        "name":"P. Witt",
+        "item":"A Bandaid and a Piece of Gum",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o72.png",
+        "name":"C. Brewer",
+        "item":"Red Pilor Razor",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o73.png",
+        "name":"M. Weinberg",
+        "item":"Plexiglass Test Cut",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o74.png",
+        "name":"J. Hendrix",
+        "item":"Bike Reflector",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o75.png",
+        "name":"S. Lee",
+        "item":"Glue Stick",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o76.png",
+        "name":"P. Masini",
+        "item":"Rubber Ring",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o77.png",
+        "name":"S. Djajasudarma",
+        "item":"Clear Tape",
+        "pancake":"pancakes/p1.png"
+    },
+    {
+        "url":"objects/o78.png",
+        "name":"E. Mogavero",
+        "item":"1/2 Sketchbook Project",
+        "pancake":"pancakes/p1.png"
+    }
+];
